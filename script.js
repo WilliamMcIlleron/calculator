387b5bf0fe1964e71e6001c5ce8db0dc522d521c
William McIlleron <williamjonahmci@icloud.com>
2025-04-07 21:17:04 +0200

    Added handling for denominator=0

--- script.js
+++ script.js
@@ -7,9 +7,31 @@ function subtract(x, y) {
 }
 
 function divide(numerator, denominator) {
+    if (+denominator === 0) return "Don't even try me... (CANNOT DIVIDE BY 0)";
     return numerator/denominator;
 }
 
 function multiply(x, y) {
     return x*y;
+}
+
+let num1;
+let symbol;
+let num2;
+
+function operate(num1, symbol, num2) {
+    switch (symbol) {
+        case 'x':
+            add(num1, num2);
+        break;
+        case '-':
+            subtract(num1, num2);
+        break;
+        case '/':
+            divide(num1, num2);
+        break;
+        case '*':
+            multiply(num1, num2);
+        break;
+    }
 }
