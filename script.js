eb16d136c22e299581c74aa9553d610d4af13ef8
William McIlleron <williamjonahmci@icloud.com>
2025-04-07 22:30:48 +0200

    Complete most of calc

--- script.js
+++ script.js
@@ -1,37 +1,102 @@
-function add(x, y) {
+const numberpad = document.querySelector("#numberpad");
+const symbolpad = document.querySelector("#symbolpad")
+const display = document.querySelector("#display")
+let num1 = "";
+let symbol;
+let num2 = "";
+let symbolClicked = false;
+
+numberpad.addEventListener("click", (e) => { 
+    let targetNum = e.target.id;
+    if (targetNum !== "numberpad") {
+        if (symbolClicked === false) {
+             num1 += targetNum;
+        } else {
+            num2 += targetNum;
+        }
+    
+        display.textContent += targetNum;
+}
+})
+
+symbolpad.addEventListener("click", (e) => {  
+    let targetSymbol = e.target.id;
+
+    if (symbolClicked === true && (targetSymbol !== "=" && targetSymbol !== "clear")) {
+        operate(num1, symbol, num2)
+        num1 = +(display.textContent);
+        num2 = "";
+        symbol = targetSymbol;
+        display.textContent += ` ${targetSymbol} `;
+        symbolClicked = true;
+        return;
+    }
+
+    if (targetSymbol !== "symbolpad") {
+        symbolClicked = true;
+        if (targetSymbol !== "=" && targetSymbol !== "clear") {
+            symbol = targetSymbol;
+            display.textContent += ` ${targetSymbol} `;
+        }
+        if (targetSymbol === "clear") {
+            display.textContent = "";
+            symbolClicked = false;
+            num1 = "";
+            num2 = "";
+            symbol = "";
+        } else if (targetSymbol === "=") {
+            operate(num1, symbol, num2);
+            symbolClicked = false;
+            num2 = "";
+            num1 = "";
+        }
+        
+    }
+})
+
+
+function add(x, y) { 
+    num2 = "";  
     return x+y;
 }
 
 function subtract(x, y) {
+    num2 = "";
     return x-y;
 }
 
 function divide(numerator, denominator) {
+    num2 = "";
     if (+denominator === 0) return "Don't even try me... (CANNOT DIVIDE BY 0)";
     return numerator/denominator;
 }
 
 function multiply(x, y) {
+    num2 = "";
     return x*y;
 }
 
-let num1;
-let symbol;
-let num2;
+
 
 function operate(num1, symbol, num2) {
+    symbolClicked = false;
+    num1 = +num1;
+    num2 = +num2;
+    console.log(num1, num2, symbol)
     switch (symbol) {
-        case 'x':
-            add(num1, num2);
+        case '+':
+            display.textContent = add(num1, num2);
         break;
         case '-':
-            subtract(num1, num2);
+            display.textContent = subtract(num1, num2);
         break;
         case '/':
-            divide(num1, num2);
+            display.textContent = divide(num1, num2);
         break;
         case '*':
-            multiply(num1, num2);
+            display.textContent = multiply(num1, num2);
         break;
     }
+    num1 = "";
+    symbol = "";
 }
