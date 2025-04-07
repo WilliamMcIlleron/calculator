7bdec65b82cd45c5ff9e7ecac718cfd9706f2c50
William McIlleron <williamjonahmci@icloud.com>
2025-04-07 20:22:02 +0200

    Make initial math functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(x, y) {
+    return x+y;
+}
+
+function subtract(x, y) {
+    return x-y;
+}
+
+function divide(numerator, denominator) {
+    return numerator/denominator;
+}
+
+function multiply(x, y) {
+    return x*y;
+}
